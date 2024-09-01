baaa6ed0bc922e3e87540a6a02977751aba97724
Brian Ratemo Magucha <brianratemom@gmail.com>
2024-09-01 15:52:18 +0300

    >>

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,16 @@
 # your code goes here!
+# lib/anagram.py
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+        # Sort the letters of the original word to compare with other words
+        self.sorted_word = ''.join(sorted(word))
+    
+    def match(self, possible_anagrams):
+        matches = []
+        for candidate in possible_anagrams:
+            # Sort the letters of the candidate word and compare with sorted_word
+            if ''.join(sorted(candidate)) == self.sorted_word:
+                matches.append(candidate)
+        return matches
